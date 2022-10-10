2d3b415b0318c9f82e45d813111172c59abe8d38
shahin azizzadeh <108829305+shahinLazycode@users.noreply.github.com>
2022-10-10 19:29:26 +0330

    Add files via upload

--- kivy army app.py
+++ kivy army app.py
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
@@ -27,7 +33,18 @@ class Score:
                     return int(row['score'])
                 elif int(row[record_title]) == self.record:
                     return int(row['score'])
-
+    
+    def find_score_with_record_time(self, title:str) -> float:
+        with open(f'{title}.csv', 'r') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for index, row in enumerate(reader):
+                if index == 0:
+                    record_title = self.find_age_title(row)
+                
+                if index == 0 and float(row[record_title]) >= self.record:
+                    return int(row['score'])
+                elif float(row[record_title]) == self.record:
+                    return int(row['score'])
     def find_age_title(self, row:dict) -> int:
         for title in row:
             try:
@@ -35,18 +52,61 @@ class Score:
                 age_title = list(map(int, age_title))
                 if age_title[0] <= self.age <= age_title[1]:
                     return title
+    
             except ValueError:
                 pass
+    
+    
+
+
+
+
+
 
 Builder.load_file('army.kv')
 
 class MyLayout(Widget):
     def press(self):
-        age = int(self.ids.sen.text)
-        enetaf = int(self.ids.enetaf.text)
-        find_score = Score(age, enetaf)
-        self.ids.enetaf_score.text = str(find_score.find_score_with_record('flexibilty'))
+        try:
+            
+            
+            age = int(self.ids.sen.text)
+            shena = int(self.ids.shena.text)
+            
+            find_shena_score = Score(age, shena)
+            self.ids.shena_score.text = str(find_shena_score.find_score_with_record('shena'))
+            
+            barfix = int(self.ids.barfix.text)
+            find_barfix_score = Score(age, barfix)
+            self.ids.barfix_score.text = str(find_barfix_score.find_score_with_record('barfix'))
+            
+            derazneshast = int(self.ids.derazneshast.text)
+            find_derazneshast_score = Score(age, derazneshast)
+            self.ids.derazneshast_score.text = str(find_derazneshast_score.find_score_with_record('derazneshast'))
+            
+            enetaf = int(self.ids.enetaf.text)
+            find_enetaf_score = Score(age, enetaf)
+            self.ids.enetaf_score.text = str(find_enetaf_score.find_score_with_record('enetaf'))
+            
+            agility = float(self.ids.agility.text)
+            find_agility_score = Score(age, agility)
+            self.ids.agility_score.text = str(find_agility_score.find_score_with_record_time('agility'))
 
+            speed = float(self.ids.speed.text)
+            find_speed_score = Score(age, speed)
+            self.ids.speed_score.text = str(find_speed_score.find_score_with_record_time('speed'))
+
+            stamina = float(self.ids.stamina.text)
+            find_stamina_score = Score(age, stamina)
+            self.ids.stamina_score.text = str(find_stamina_score.find_score_with_record_time('stamina'))
+
+        except ValueError:
+                pass 
+    def clear(self):
+        self.ids.shena.text==0
+        
+
+        
 class ArmyApp(App):
     def build(self):
         return MyLayout()
