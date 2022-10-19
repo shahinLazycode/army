00caaf11920e0af808ba6870c3c874c66f97e0ca
Abbas Rahimzadeh <arahimzadeh79@gmail.com>
2022-10-20 01:35:50 +0330

    add try except in MyLayout class

--- kivy army app.py
+++ kivy army app.py
@@ -1,9 +1,3 @@
-
-
-
-
-
-
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
@@ -28,7 +22,6 @@ class Score:
             for index, row in enumerate(reader):
                 if index == 0:
                     record_title = self.find_age_title(row)
-                
                 if index == 0 and int(row[record_title]) <= self.record:
                     return int(row['score'])
                 elif int(row[record_title]) == self.record:
@@ -40,11 +33,11 @@ class Score:
             for index, row in enumerate(reader):
                 if index == 0:
                     record_title = self.find_age_title(row)
-                
                 if index == 0 and float(row[record_title]) >= self.record:
                     return int(row['score'])
                 elif float(row[record_title]) == self.record:
                     return int(row['score'])
+
     def find_age_title(self, row:dict) -> int:
         for title in row:
             try:
@@ -52,56 +45,67 @@ class Score:
                 age_title = list(map(int, age_title))
                 if age_title[0] <= self.age <= age_title[1]:
                     return title
-    
             except ValueError:
                 pass
-    
-    
-
-
-
-
-
 
 Builder.load_file('army.kv')
 
 class MyLayout(Widget):
     def press(self):
         try:
-            
-            
             age = int(self.ids.sen.text)
-            shena = int(self.ids.shena.text)
-            
-            find_shena_score = Score(age, shena)
-            self.ids.shena_score.text = str(find_shena_score.find_score_with_record('shena'))
-            
-            barfix = int(self.ids.barfix.text)
-            find_barfix_score = Score(age, barfix)
-            self.ids.barfix_score.text = str(find_barfix_score.find_score_with_record('barfix'))
-            
-            derazneshast = int(self.ids.derazneshast.text)
-            find_derazneshast_score = Score(age, derazneshast)
-            self.ids.derazneshast_score.text = str(find_derazneshast_score.find_score_with_record('derazneshast'))
-            
-            enetaf = int(self.ids.enetaf.text)
-            find_enetaf_score = Score(age, enetaf)
-            self.ids.enetaf_score.text = str(find_enetaf_score.find_score_with_record('enetaf'))
-            
-            agility = float(self.ids.agility.text)
-            find_agility_score = Score(age, agility)
-            self.ids.agility_score.text = str(find_agility_score.find_score_with_record_time('agility'))
-
-            speed = float(self.ids.speed.text)
-            find_speed_score = Score(age, speed)
-            self.ids.speed_score.text = str(find_speed_score.find_score_with_record_time('speed'))
-
-            stamina = float(self.ids.stamina.text)
-            find_stamina_score = Score(age, stamina)
-            self.ids.stamina_score.text = str(find_stamina_score.find_score_with_record_time('stamina'))
-
         except ValueError:
-                pass 
+            age = 0
+        
+        try:
+            shena = int(self.ids.shena.text)
+        except ValueError:
+            shena = 0
+        find_shena_score = Score(age, shena)
+        self.ids.shena_score.text = str(find_shena_score.find_score_with_record('shena'))
+            
+        try:
+            barfix = int(self.ids.barfix.text)
+        except ValueError:
+            barfix = 0
+        find_barfix_score = Score(age, barfix)
+        self.ids.barfix_score.text = str(find_barfix_score.find_score_with_record('barfix'))
+            
+        try:
+            derazneshast = int(self.ids.derazneshast.text)
+        except ValueError:
+            derazneshast = 0
+        find_derazneshast_score = Score(age, derazneshast)
+        self.ids.derazneshast_score.text = str(find_derazneshast_score.find_score_with_record('derazneshast'))
+            
+        try:
+            enetaf = int(self.ids.enetaf.text)
+        except ValueError:
+            enetaf = 0
+        find_enetaf_score = Score(age, enetaf)
+        self.ids.enetaf_score.text = str(find_enetaf_score.find_score_with_record('enetaf'))
+            
+        try:
+            agility = float(self.ids.agility.text)
+        except ValueError:
+            agility = 0
+        find_agility_score = Score(age, agility)
+        self.ids.agility_score.text = str(find_agility_score.find_score_with_record_time('agility'))
+
+        try:
+            speed = float(self.ids.speed.text)
+        except ValueError:
+            speed = 0
+        find_speed_score = Score(age, speed)
+        self.ids.speed_score.text = str(find_speed_score.find_score_with_record_time('speed'))
+
+        try:
+            stamina = float(self.ids.stamina.text)
+        except ValueError:
+            stamina = 0
+        find_stamina_score = Score(age, stamina)
+        self.ids.stamina_score.text = str(find_stamina_score.find_score_with_record_time('stamina'))
+    
     def clear(self):
         self.ids.shena.text==0
         
